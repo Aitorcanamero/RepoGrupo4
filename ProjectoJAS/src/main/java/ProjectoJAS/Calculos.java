3f7d29a83f7d7d7d5038ecdc9985f0220e6f6668
in2daw <in2daw@elorrieta.int>
2020-10-19 20:36:57 +0200

    actualizacion resta

--- ProjectoJAS/src/main/java/ProjectoJAS/Calculos.java
+++ ProjectoJAS/src/main/java/ProjectoJAS/Calculos.java
@@ -3,15 +3,20 @@ package ProjectoJAS;
 public class Calculos {
 
 
-	public int multiplicacion(int x, int y) {
-		return x*y;
-		
-	}
-
-
 public int suma (int x ,int y) {
-	
 	
 	return x+y;
 }
+
+public int resta (int x, int y) {
+	
+	return x-y;
+}
+
+public int multiplicacion(int x, int y) {
+	
+	return x*y;
+	
+}
+
 }
